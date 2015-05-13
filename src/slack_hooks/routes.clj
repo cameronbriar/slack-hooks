d1ea050fff786abbaaa39dd9295e92676dfe4f3e
David Farr <david.farr@sap.com>
2015-05-13 16:27:06 -0700

    Add def-event macro

--- src/slack_hooks/routes.clj
+++ src/slack_hooks/routes.clj
@@ -5,28 +5,49 @@
 
 (def EVENTS {})
 
-(defn def-event [name fn]
+(defn fire [event args]
+  (if (contains? EVENTS event)
+    ((get EVENTS event) args)
+    (str "Unsupported event")))
+
+(defn def-event-fn [name fn]
   (def EVENTS (conj EVENTS {name fn})))
 
-(def-event "star" #(println %1))
+(defmacro def-event [name args & fn]
+  `(def-event-fn ~name (fn ~args ~@fn)))
+
+(def-event "star" [{:keys [username number text stars threshold]}]
+  (str username " has starred #" number ": " text "\n"
+       "#" number " has " stars " of " threshold " needed stars."))
+
+(def-event "unstar" [{:keys [username number text stars threshold]}]
+  (str username " has unstarred #" number ": " text "\n"
+       "#" number " has " stars " of " threshold " needed stars."))
+
+(def-event "pr-create" [{:keys [username number text threshold]}]
+  (str username " has created pull request #" number ": " text "\n"
+       "#" number " needs " threshold " stars to merge."))
+
+(def-event "pr-merge" [{:keys [username number text]}]
+  (str username " has merged #" number ": " text "!"))
 
 (defn home-page []
   "ReviewNinja + Slack = <3")
 
-(defn receive [{:keys [params]}]
-  (let [event (get params :event)
-        token (get params :token)
+(defn handle [{:keys [params]}]
+  (let [event   (get params :event)
+        token   (get params :token)
         channel (get params :channel "#general")
-        args {:username (get-in params [:sender :login])
-              :pr-number (get-in params [:pull_request :number])
-              :pr-text (get-in params [:pull_request :title])
-              :pr-stars (get-in params [:pull_request :stars])
-              :pr-threshold (get-in params [:pull_request :threshold])}]
-    (println params)
-    (println event token channel args)
-    (api/send-message token channel ((get EVENTS event) args))
-    ((get EVENTS event) 1 2)))
+        args {:username  (get-in params [:sender :login])
+              :number    (get-in params [:pull_request :number])
+              :text      (get-in params [:pull_request :title])
+              :stars     (get-in params [:pull_request :stars])
+              :threshold (get-in params [:pull_request :threshold])}]
+    (def text (fire event args))
+    (if (not= text "Unsupported event")
+      (api/send-message token channel text))
+    (str text)))
 
 (defroutes home-routes
   (GET "/" [] (home-page))
-  (POST "/" request (receive request)))
+  (POST "/" request (handle request)))
